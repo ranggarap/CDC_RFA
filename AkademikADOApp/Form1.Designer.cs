a1f0a0fa2ab34388336369e1a8ab221fa59ce839
ranggarap <fadhilahrangga26@gmail.com>
2026-03-10 12:41:44 +0700

    membuat tabel connect

--- AkademikADOApp/Form1.Designer.cs
+++ AkademikADOApp/Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnconnect = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnconnect
             // 
-            this.button1.Location = new System.Drawing.Point(506, 312);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnconnect.Location = new System.Drawing.Point(506, 312);
+            this.btnconnect.Name = "btnconnect";
+            this.btnconnect.Size = new System.Drawing.Size(195, 25);
+            this.btnconnect.TabIndex = 0;
+            this.btnconnect.Text = "connect Database";
+            this.btnconnect.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnconnect);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +54,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnconnect;
     }
 }
 
